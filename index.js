a84996a7f64a72b56da7ebc39ab8166914da9415
Briana Oniru <damioniru16@gmail.com>
2020-08-09 23:25:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,21 +10,27 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.floor(Math.random() * 100) + 1;
+  var item_object = {};
+  item_object[item] = price
+  cart.push(item_object)
+  console.log(item + " has been added to your cart.");
+  return cart
 }
 
+
 function viewCart() {
-  // write your code here
+
 }
 
 function total() {
-  // write your code here
+ 
 }
 
 function removeFromCart(item) {
-  // write your code here
+ 
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+ 
 }
